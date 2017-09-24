f6e4300983cbfba85de06101cb475bb3f9163aaa
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-09-24 15:09:29 +0200

    refactored image load and display the first n images

---
+++ task_01_2.m
@@ -0,0 +1,16 @@
+# Load the train images and labels.
+[images, labels] = load_minst_database('data/train-images-idx3-ubyte', 'data/train-labels-idx1-ubyte');
+
+# Display the first n images.
+
+# Set the total number of images to plot.
+n = 4;
+
+# Set the number of images per row.
+m = 2;
+
+for i = 1:n
+  subplot(ceil(n/m),m,i), imshow(uint8(images(:,:,i)))
+end
+
+clear;
