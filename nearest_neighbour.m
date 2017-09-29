a962f611597d234173c746844495760572f10d08
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2017-09-29 15:05:21 +0200

    added progressbar and classification results to nearest neighbour

--- nearest_neighbour.m
+++ nearest_neighbour.m
@@ -1,7 +1,12 @@
-function nearest_neighbour(num_of_train_data, num_of_test_data, show_log)
+ function nearest_neighbour(num_of_train_data, num_of_test_data, show_log)
   %num_of_train_data: The number of training images used to classify one test image.
   %num_of_test_data: The number of test images to classify.
   %show_log: 1 or 0 to specify whether intermediate steps should be printed to the console or not.
+  
+  % Display waitbar for showing progress.
+  h = waitbar(0,'Preparing ...','Name','Classify Test Images',...
+            'CreateCancelBtn',...
+            'setappdata(gcbf,''canceling'',1)');
   
   % Load the minst train and test datasets.
   if (show_log == 1)
@@ -15,83 +20,99 @@ function nearest_neighbour(num_of_train_data, num_of_test_data, show_log)
   % n = num_of_test_data
   % Classify the first n images of the minst test dataset.
   for i = 1:num_of_test_data
-      % Print the number of the image that is currently classified.
-      if (show_log == 1)
-        fprintf('%03d - ', i);
-      end
-      
-      % Get the image to be classified.
-      test_image = test_data(:,:,i); 
-      
-      % Transpose the test image matrix into a vector.
-      test_image = test_image(:); 
+    % Check for Cancel button press.
+    if getappdata(h,'canceling')
+        fprintf('Aborted!\n');
+        break
+    end
     
-      % Minimal found (euclidean) distance between the test and all of the train
-      % images.
-      min_dist = Inf;
-      
-      % Index of the test image with the minimal distance to the test image.
-      min_index = 0;
-      
-      % m = num_of_train_data
-      % Run through the first m images of the minst train dataset to classify
-      % the test image.
-      for j = 1:num_of_train_data
-          % Get the train image.
-          train_image = train_data(:,:,j);
-          
-          % Transpose the test image matrix into a vector.
-          train_image = train_image(:);
-          
-          % Get the diffrence between the test and train image vectors.
-          diff = test_image - train_image;
-          
-          % Convert difference image from 'uint-8' into double.
-          diff = im2double(diff);
-          
-          % Get the euclidean norm of the test and train image vectors.
-          dist = norm(diff,2); 
-          
-          % Reset the minimal found euclidean distance.
-          if(dist < min_dist)
-              min_dist = dist;
-              min_index = j;
-          end
-      end
-      
-      % Get the label of the train image with the lowest euclidean difference
-      % to the test image.
-      result(i) = train_labels(min_index); 
-      
-      % Check if the resulting label is the same as expected.
-      if (test_labels(i) ~= result(i))
-          errors = errors + 1;
-      end
-      
-      % Calculate and print the current accuracy.
-      current_accuracy = (i - errors) / i;
+    % Print progress.
+    progress = 100/100/num_of_test_data*i;
+    waitbar(progress, h, sprintf('%d%%',progress*100));
+  
+    % Print the number of the image that is currently classified.
+    if (show_log == 1)
+      fprintf('%03d - ', i);
+    end
+    
+    % Get the image to be classified.
+    test_image = test_data(:,:,i); 
+    
+    % Transpose the test image matrix into a vector.
+    test_image = test_image(:); 
+  
+    % Minimal found (euclidean) distance between the test and all of the train
+    % images.
+    min_dist = Inf;
+    
+    % Index of the test image with the minimal distance to the test image.
+    min_index = 0;
+    
+    % m = num_of_train_data
+    % Run through the first m images of the minst train dataset to classify
+    % the test image.
+    for j = 1:num_of_train_data
+        % Get the train image.
+        train_image = train_data(:,:,j);
+        
+        % Transpose the test image matrix into a vector.
+        train_image = train_image(:);
+        
+        % Get the diffrence between the test and train image vectors.
+        diff = test_image - train_image;
+        
+        % Convert difference image from 'uint-8' into double.
+        diff = im2double(diff);
+        
+        % Get the euclidean norm of the test and train image vectors.
+        dist = norm(diff,2); 
+        
+        % Reset the minimal found euclidean distance.
+        if(dist < min_dist)
+            min_dist = dist;
+            min_index = j;
+        end
+    end
+    
+    % Get the label of the train image with the lowest euclidean difference
+    % to the test image.
+    result(i) = train_labels(min_index); 
+    
+    % Check if the resulting label is the same as expected.
+    if (test_labels(i) ~= result(i))
+        errors = errors + 1;
+    end
+    
+    % Calculate and print the current accuracy.
+    current_accuracy = (i - errors) / i;
+    if (show_log == 1)
+      fprintf('Current Accuracy: %f - ', current_accuracy);
+    end
+    
+    % If the guess was not correct
+    if (test_labels(i) ~= result(i))
       if (show_log == 1)
-        fprintf('Current Accuracy: %f - ', current_accuracy);
+          fprintf('NO MATCH -  Test label: %d, Training Label %d.\n', test_labels(i), result(i));
       end
-      
-      % If the guess was not correct
-      if (test_labels(i) ~= result(i))
-        if (show_log == 1)
-            fprintf('NO MATCH -  Test label: %d, Training Label %d.\n', test_labels(i), result(i));
-        end
-          
-        % Uncomment to display the test and train images.
-        %figure;
-        %images = [test_data(:,:,i) train_data(:,:,min_index)];
-        %imshow(images);
-      else
-        if (show_log == 1)
-          fprintf('MATCH\n');
-        end
+        
+      % Uncomment to display the test and train images.
+      %figure;
+      %images = [test_data(:,:,i) train_data(:,:,min_index)];
+      %imshow(images);
+    else
+      if (show_log == 1)
+        fprintf('MATCH\n');
       end
+    end
   end
+  waitbar(1, h, 'Finishing ...');
   
   % Print the total accuracy.
-  fprintf('Tested images: %d | Matches: %d | Mismatches: %d\n', num_of_test_data, num_of_test_data-errors, errors);
-  fprintf('Total Accuracy: %d%%\n', round(current_accuracy*100*100)/100);
+  %fprintf('Tested images: %d | Matches: %d | Mismatches: %d\n', num_of_test_data, num_of_test_data-errors, errors);
+  %fprintf('Total Accuracy: %d%%\n', round(current_accuracy*100*100)/100);
+  %fprintf('Total Error Rate: %d%%\n', 100 - round(current_accuracy*100*100)/100);
+  fprintf('Train images: %d | Test Images: %d | Error Rate: %d%%\n', num_of_train_data, num_of_test_data, 100 - round(current_accuracy*100*100)/100)
+
+  % Close waitbar.
+  close(h);
 end
--- task_01_3.m
+++ task_01_3.m
@@ -1,9 +1,9 @@
 # Set the number of train images that should be used to classify each of the
-# test images (max 10'000).
-train_size = 1000;
+# test images (max 60'000).
+train_size = 60000;
 
-# Set the number of test images that should be classified (max 60'000).
-test_size = 100;
+# Set the number of test images that should be classified (max 10'000).
+test_size = 1000;
 
 # Specify whether the result of each image classification should be printed or not.
 show_log = 0;
@@ -11,3 +11,24 @@ show_log = 0;
 nearest_neighbour(train_size, test_size, show_log);
 
 clear;
+
+% Best Result:
+% ?Train images: 10000 | Test Images: 100 | Error Rate: 19%
+
+% All Results:
+% Train images: 10 | Test Images: 10 | Error Rate: 70%
+% Train images: 100 | Test Images: 10 | Error Rate: 40%
+% Train images: 1000 | Test Images: 10 | Error Rate: 40%
+% Train images: 10000 | Test Images: 10 | Error Rate: 30%
+% Train images: 60000 | Test Images: 10 | Error Rate: 20%
+
+% Train images: 10 | Test Images: 100 | Error Rate: 64%
+% Train images: 100 | Test Images: 100 | Error Rate: 35%
+% Train images: 1000 | Test Images: 100 | Error Rate: 40%
+% ?Train images: 10000 | Test Images: 100 | Error Rate: 19%
+% Train images: 60000 | Test Images: 100 | Error Rate: 22%
+
+% Train images: 10 | Test Images: 1000 | Error Rate: 63.3%
+% Train images: 100 | Test Images: 1000 | Error Rate: 42%
+% Train images: 1000 | Test Images: 1000 | Error Rate: 39.1%
+% Train images: 10000 | Test Images: 1000 | Error Rate: 30.9%
